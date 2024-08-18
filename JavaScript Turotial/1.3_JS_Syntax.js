0bb3b54c7106e165984391f6721b259af2a742b8
Masud Parvez <masudparvez2050@gmail.com>
2024-08-18 21:37:27 +0600

    add variable.js

---
+++ JavaScript Turotial/1.3_JS_Syntax.js
@@ -0,0 +1,55 @@
+// JavaScript Systax
+
+// How to Create a variables:
+
+// Automatic init
+x;
+// y;
+// z;
+
+// Manual init
+var x;
+let y;
+let z;
+
+// Assign value
+
+var x;
+x = 5;
+var x = 5;
+let q = 6;
+const Z = 10;
+
+// operator
+
+var x = 10 + 20;
+var x = 10 * 20;
+var x = 10 / 20;
+var x = 10 - 20;
+
+// condition
+
+if (true) {
+  console.log("Hello");
+}
+
+if (x > 5) {
+  console.log("Greater than 5");
+} else {
+  console.log("Less than or equal to 5");
+}
+
+// Ternary Operator
+
+var x = 10;
+var result = x < 5 ? "Greater than 5" : "Less than or equal to 5";
+
+console.log(result);
+
+// function
+
+function myFunc() {
+  console.log("masud parvez");
+}
+
+myFunc();
---
+++ JavaScript Turotial/2.0_JS_variables.js
@@ -0,0 +1,50 @@
+// JavaScript Variables
+
+// javaScript Variables can be declared in 4 ways:
+
+/* 
+1. Automatically
+2. using "var"
+3. using "let"
+4. using "const"
+
+*/
+
+// 1. Automatically assigned a value
+x = 5;
+y = 6;
+z = x + y;
+
+console.log(z);
+
+// 2. Using "var"
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+console.log(z);
+
+// 3. Using "let"
+
+let a = 5;
+let b = 6;
+let c = a + b;
+
+console.log(z);
+
+// 4. Using "const"
+
+const d = 5;
+const e = 6;
+const f = d + e;
+
+console.log(f);
+
+// Mixed Example
+
+let g = 5;
+const h = 6;
+
+g = g + h;
+console.log(g); // Outputs: 11
